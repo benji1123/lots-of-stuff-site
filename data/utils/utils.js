9a8fafb605445a9cedf73a2c9bb841e7d0b91e50
Ben <benji112358@gmail.com>
2025-06-29 22:00:07 -0700

    fix: date util not returning in local Vancouver time

--- data/utils/utils.js
+++ data/utils/utils.js
@@ -1,7 +1,5 @@
 const DEFAULT_TIMEZONE = "America/Vancouver";
-const DEFAULT_LOCALE = "en-US";
 
 export const getCurrentDate = (timeZone = DEFAULT_TIMEZONE) => {
-    const now = new Date();
-    return new Date(now.toLocaleString(DEFAULT_LOCALE, { timeZone })).toISOString().slice(0, 10);
-}
+    return new Date().toLocaleDateString('en-CA', { timeZone }); // 'YYYY-MM-DD'
+};
