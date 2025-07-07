b87c8a7be3fbe58b5cf1c683b7b767270d602f1b
Ben <benji112358@gmail.com>
2025-07-06 23:46:50 -0700

    add sunset/sunrise/dusk to status bar

--- steam-app/src/components/StatusBar.tsx
+++ steam-app/src/components/StatusBar.tsx
@@ -12,21 +12,55 @@ export default function StatusBar() {
         'waning crescent': '🌘'
 
     }
-    const [moon, setMoon] = useState('')
+    const [moon, setMoon] = useState<any>({ Phase: '' })
+    const[dayData, setDayData] = useState({ sunset: '', sunrise: '', dusk: '' })
 
-    const unixTimeNow = Date.now();
+    const unixTime = Math.floor(Date.now() / 1000); // UTC seconds
+    console.log(`time: ${unixTime}`)
 
+    // fetch moon phase - https://www.farmsense.net/api/astro-widgets/
     useEffect(() => {
-        fetch(`https://api.farmsense.net/v1/moonphases/?d=${unixTimeNow}`)
+        fetch(`https://api.farmsense.net/v1/moonphases/?d=${unixTime}`)
             .then(res => res.json())
-            .then(res => res[0].Phase.toLowerCase())
+            .then(res => res[0])
             .then(setMoon)
-            .then(res => console.log(`moon phase: ${res}`))
+            .then(res => console.log(`moon: ${res}`))
+    }, [])
+
+    // fetch sunset and sunrise - https://sunrisesunset.io/api/
+    const vancouverCoord = {
+        latitude: 49.246292,
+        longitude: -123.116226
+    }
+    useEffect(() => {
+        fetch(`https://api.sunrisesunset.io/json?lat=${vancouverCoord.latitude}&lng=${vancouverCoord.longitude}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(JSON.stringify(res))
+                const data = res.results
+                setDayData(data);
+            })
+            .then(res => console.log(`day data: ${res}`))
     }, [])
 
     return (
-        <div className="flex px-[1.5em] h-[50px] w-viewport bg-black/10 rounded-sm items-center text-md text-gray-500">
-            {moon}: {moonPhaseMap[moon]}
+        <div className="flex gap-[2em] px-[1.5em] h-[50px] w-viewport bg-black/10 rounded-sm items-center text-md text-gray-500">
+            <div>
+                {moonPhaseMap[moon.Phase.toLowerCase()]} {moon.Phase.toLowerCase()}
+            </div>
+
+            <div>
+                🌅 {dayData.sunrise}
+            </div>
+
+            <div>
+                🌇 {dayData.sunset}
+            </div>
+
+            <div>
+                🌆 {dayData.dusk}
+            </div>
         </div>
+        
     )
 }
