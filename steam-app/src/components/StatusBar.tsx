8bdac332e7da3d8a2c9e76555a2479208ce2f582
Ben <benji112358@gmail.com>
2025-07-07 01:24:42 -0700

    add weather to status bar

--- steam-app/src/components/StatusBar.tsx
+++ steam-app/src/components/StatusBar.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, ReactNode } from "react"
+import { OpenMeteoCurrentWeather } from "../data/types"
 
 export default function StatusBar() {
     const moonPhaseMap: Record<string, string> = {
@@ -14,11 +15,12 @@ export default function StatusBar() {
     }
     const [moon, setMoon] = useState<any>({ Phase: '' })
     const[dayData, setDayData] = useState({ sunset: '', sunrise: '', dusk: '' })
+    const [weather, setWeather] = useState<OpenMeteoCurrentWeather | any>({})
 
     const unixTime = Math.floor(Date.now() / 1000); // UTC seconds
     console.log(`time: ${unixTime}`)
 
-    // fetch moon phase - https://www.farmsense.net/api/astro-widgets/
+    // fetch moon phase - https://www.farmsense.net/api/astro-widgets/ (public)
     useEffect(() => {
         fetch(`https://api.farmsense.net/v1/moonphases/?d=${unixTime}`)
             .then(res => res.json())
@@ -27,7 +29,7 @@ export default function StatusBar() {
             .then(res => console.log(`moon: ${res}`))
     }, [])
 
-    // fetch sunset and sunrise - https://sunrisesunset.io/api/
+    // fetch sunset and sunrise - https://sunrisesunset.io/api/ (public)
     const vancouverCoord = {
         latitude: 49.246292,
         longitude: -123.116226
@@ -40,27 +42,75 @@ export default function StatusBar() {
                 const data = res.results
                 setDayData(data);
             })
-            .then(res => console.log(`day data: ${res}`))
+    }, [])
+
+    // Fetch weather = https://open-meteo.com/en/docs?latitude=49.246292&longitude=-123.116226&timezone=auto&hourly=&forecast_days=1&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m#settings (public)
+    useEffect(() => {
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=49.246292&longitude=-123.116226&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m&timezone=auto&forecast_days=1`)
+        .then(res => res.json())
+        .then(setWeather)
     }, [])
 
     return (
-        <div className="flex gap-[2em] px-[1.5em] h-[50px] w-viewport bg-black/10 rounded-sm items-center text-md text-gray-500">
-            <div>
-                {moonPhaseMap[moon.Phase.toLowerCase()]} {moon.Phase.toLowerCase()}
-            </div>
+        <div className="flex gap-[1em] px-[1.5em] h-[50px] w-viewport rounded-sm items-center text-md text-gray-400">
+            {/* moon-phase and sunset/sunrise/dusk */}
+            <StatusBarContainer>
+                <div>{moonPhaseMap[moon.Phase.toLowerCase()]} {moon.Phase.toLowerCase()}</div>
+            </StatusBarContainer>
 
-            <div>
-                🌅 {dayData.sunrise}
-            </div>
+            <StatusBarContainer>
+                <div>🌅 {dayData.sunrise}</div>
+                <div>🌇 {dayData.sunset}</div>
+                <div>🌆 {dayData.dusk}</div>
+            </StatusBarContainer>
+            
+            {/* weather */}
+            <StatusBarContainer>
+                🗜 {weather?.current?.pressure_msl}{weather?.current_units?.pressure_msl}
+            </StatusBarContainer>
 
-            <div>
-                🌇 {dayData.sunset}
-            </div>
-
-            <div>
-                🌆 {dayData.dusk}
-            </div>
+            <StatusBarContainer>
+                <div>
+                {weather?.current?.temperature_2m}{weather?.current_units?.temperature_2m}
+                </div>
+                <div>
+                    💦 {weather?.current?.rain}{weather?.current_units?.rain}
+                </div>
+                <div>
+                    🌪️ {weather?.current?.wind_speed_10m}{weather?.current_units?.wind_speed_10m} {degToCardinal(weather?.current?.wind_direction_10m)}
+                </div>
+            </StatusBarContainer>
         </div>
         
     )
+}
+
+const degToCardinal = (degrees: number): string => {
+  const cardinalDirections = [
+    "N",
+    "NE",
+    "E",
+    "SE",
+    "S",
+    "SW",
+    "W",
+    "NW"
+  ];
+  const degPerDirection = 360 / cardinalDirections.length;
+  const index = Math.round(degrees / degPerDirection);
+  return cardinalDirections[Math.min(index, cardinalDirections.length-1)];
+}
+
+
+type StatusBarContainerProps = {
+  color?: string;
+  children: ReactNode;
+};
+
+function StatusBarContainer({ color = "bg-black/85", children }: StatusBarContainerProps) {
+  return (
+    <div className={`flex gap-[1em] text-xs md:text-sm whitespace-nowrap px-[1.5em] py-2 rounded-full text-gray-400 ${color}`}>
+      {children}
+    </div>
+  );
 }
--- steam-app/src/data/types.ts
+++ steam-app/src/data/types.ts
@@ -8,3 +8,45 @@ export interface ActivityEntry {
   color?: string;
   [key: string]: any; 
 }
+
+// https://open-meteo.com/en/docs?latitude=49.246292&longitude=-123.116226&timezone=auto&hourly=&forecast_days=1&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m#settings
+export type OpenMeteoCurrentWeather = {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_units: {
+    time: string;
+    interval: string;
+    temperature_2m: string;
+    relative_humidity_2m: string;
+    is_day: string;
+    precipitation: string;
+    rain: string;
+    weather_code: string;
+    cloud_cover: string;
+    pressure_msl: string;
+    surface_pressure: string;
+    wind_speed_10m: string;
+    wind_direction_10m: string;
+  };
+  current: {
+    time: string;
+    interval: number;
+    temperature_2m: number;
+    relative_humidity_2m: number;
+    is_day: number;
+    precipitation: number;
+    rain: number;
+    weather_code: number;
+    cloud_cover: number;
+    pressure_msl: number;
+    surface_pressure: number;
+    wind_speed_10m: number;
+    wind_direction_10m: number;
+  };
+  any: any; // in case more fields are added
+};
